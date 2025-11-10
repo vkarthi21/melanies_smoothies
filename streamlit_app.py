0458622298d7d60165ca3c4637e9ad268b816718
vkarthi21 <101750921+vkarthi21@users.noreply.github.com>
2025-11-10 12:13:51 -0800

    Update streamlit_app.py
    converting snowflake to pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+#import pandas as pd
 import streamlit as st 
 from snowflake.snowpark.functions import col
 import requests
@@ -20,7 +21,12 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#Convert the Snowpark Datafram to a Pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect('Chose up to 5 ingredients:'
